b3071286c1b420c081a70293926f22361c8f6c5e
myhappinesszhang <2189247923@qq.com>
2021-12-22 09:25:14 +0800

    完成了demo02

--- src/main/java/cn/ty/controller/demo01.java
+++ src/main/java/cn/ty/controller/demo01.java
@@ -2,8 +2,8 @@ package cn.ty.controller;
 
 public class demo01 {
     public static void main(String[] args) {
-        System.out.println("11111");
+        System.out.println("1112211");
         System.out.println("333");
-        System.out.println("777777777");
+        System.out.println("ad");
     }
 }
