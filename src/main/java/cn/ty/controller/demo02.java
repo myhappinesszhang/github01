ccc0a894dd2bb7e19fce105abd6c0e442ffdb5cd
myhappinesszhang <2189247923@qq.com>
2021-12-22 08:34:32 +0800

    完成了demo02

---
+++ src/main/java/cn/ty/controller/demo02.java
@@ -0,0 +1,11 @@
+package cn.ty.controller;
+
+public class demo02 {
+    public static void main(String[] args) {
+        for (int i = 0; i <100 ; i++) {
+            System.out.println(i);
+
+        }
+        System.out.println("子航6666");
+    }
+}
