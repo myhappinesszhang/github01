0352ca369051a2e193bf06d14eae720e526a92b9
myhappinesszhang <96402051+myhappinesszhang@users.noreply.github.com>
2021-12-22 09:28:56 +0800

    Update demo01.java
    aazz

--- src/main/java/cn/ty/controller/demo01.java
+++ src/main/java/cn/ty/controller/demo01.java
@@ -5,5 +5,7 @@ public class demo01 {
         System.out.println("1112211");
         System.out.println("333");
         System.out.println("ad");
+        System.out.println("zz");
+        System.out.println("xx");
     }
 }
